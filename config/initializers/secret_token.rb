dfff6b21b96f78392323f15b2f64662b5dfbfa1f
djangogeek <laurent.benedetti@gmail.com>
2013-11-18 11:53:38 +0100

    fgfhg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokest::Application.config.secret_token = '56f9df465bca55a534be3c069a12008c5ca9f62e9fc143dba567e8643a978278980be087fd23a10f87ca039e96cce8ec1d978fd8507a6d09b600164d6b697e91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Herokest::Application.config.session_store :cookie_store, key: '_herokest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Herokest::Application.config.session_store :active_record_store
